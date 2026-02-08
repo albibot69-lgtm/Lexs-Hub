8cd448c05e4168857e5b90b61787cfe3ab255d18
LexsHubNew <albibot69@gmail.com>
2026-02-08 13:35:28 +0700

    Update and rename Premium.lua to Premium.lua

---
+++ WebhookETFB/Premium.lua
@@ -0,0 +1,64 @@
+local HttpService = game:GetService("HttpService")
+
+local WEBHOOK = "https://discord.com/api/webhooks/1464548671097016485/bCnoCBLKlTA17Etcq7Vl4G2XVaUBacpcBJZvoxxtFuUoAMRg4vEzGF7Bl1DMgtkkfiG6"
+
+-- ROLE MENTION
+local ROLES = {
+	"1421127844821602426",
+}
+
+-- VERSION DINAMIS
+local VERSION_TEXT = "Premium"
+
+-- BUILD MENTION
+local mentions = {}
+for _, id in ipairs(ROLES) do
+	table.insert(mentions, "<@&"..id..">")
+end
+
+local data = {
+	username = "Lexs Hub Update",
+	avatar_url = "https://cdn.discordapp.com/attachments/1427988930103476225/1463860113470259277/lexshubicon.png",
+
+	content = table.concat(mentions, " "),
+
+	allowed_mentions = {
+		roles = ROLES
+	},
+
+	embeds = {{
+		title = "Escape Tsunami For Brainrot",
+
+		author = {
+			name = "Version: "..VERSION_TEXT
+		},
+
+		color = 0x00c3ff,
+
+		description =
+			"```diff\n" ..
+			"[ + ] Added Disable Cutscene\n" ..
+            "[ / ] Fix Instant Fishing\n" ..
+			"```\n\n" ..
+			"**Rejoin & Re-execute the script**",
+
+		image = {
+			url = "https://tr.rbxcdn.com/180DAY-c90011b352b6cbbbc8fb34d3aac447c2/768/432/Image/Webp/noFilter"
+		},
+
+		footer = {
+			text = "Lexs Hub Update • Escape Tsunami For Brainrot"
+		},
+
+		timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
+	}}
+}
+
+request({
+	Url = WEBHOOK,
+	Method = "POST",
+	Headers = {
+		["Content-Type"] = "application/json"
+	},
+	Body = HttpService:JSONEncode(data)
+})
