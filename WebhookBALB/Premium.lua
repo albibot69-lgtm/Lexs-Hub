98dc51cd36874d42f0469a79bd28ad4a4fd9b00f
LexsHubNew <albibot69@gmail.com>
2026-02-10 19:52:26 +0700

    Update Premium.lua

--- WebhookBALB/Premium.lua
+++ WebhookBALB/Premium.lua
@@ -18,7 +18,7 @@ end
 
 local data = {
 	username = "Lexs Hub Update",
-	avatar_url = "https://cdn.discordapp.com/attachments/1427988930103476225/1463860113470259277/lexshubicon.png",
+	avatar_url = "https://cdn.discordapp.com/attachments/1460885261616550015/1469958871488528530/LexsHub3Decal.png?ex=698c3061&is=698adee1&hm=f466d9bb7d3f85d6788c5ed77a2ff8860003121a92095768c142af3561e05b53&",
 
 	content = table.concat(mentions, " "),
 
