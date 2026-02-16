4962a1ad9376be20dcfb25912a45e2299579fcd6
LexsHubNew <albibot69@gmail.com>
2026-02-16 14:38:26 +0700

    Create Freemium.lua

---
+++ WebhookRAS/Freemium.lua
@@ -0,0 +1,63 @@
+local HttpService = game:GetService("HttpService")
+
+local WEBHOOK = "https://discord.com/api/webhooks/1464548671097016485/bCnoCBLKlTA17Etcq7Vl4G2XVaUBacpcBJZvoxxtFuUoAMRg4vEzGF7Bl1DMgtkkfiG6"
+
+-- ROLE MENTION
+local ROLES = {
+	"1421127844821602426",
+}
+
+-- VERSION DINAMIS
+local VERSION_TEXT = "Freemium"
+
+-- BUILD MENTION
+local mentions = {}
+for _, id in ipairs(ROLES) do
+	table.insert(mentions, "<@&"..id..">")
+end
+
+local data = {
+	username = "Lexs Hub Update",
+	avatar_url = "https://cdn.discordapp.com/attachments/1460885261616550015/1469958871488528530/LexsHub3Decal.png?ex=698c3061&is=698adee1&hm=f466d9bb7d3f85d6788c5ed77a2ff8860003121a92095768c142af3561e05b53&",
+
+	content = table.concat(mentions, " "),
+
+	allowed_mentions = {
+		roles = ROLES
+	},
+
+	embeds = {{
+		title = "Reborn As Swordsman",
+
+		author = {
+			name = "Version: "..VERSION_TEXT
+		},
+
+		color = 0x00c3ff,
+
+		description =
+			"```diff\n" ..
+			"[ + ] Release\n" ..
+			"```\n\n" ..
+			"**Rejoin & Re-execute the script**",
+
+		image = {
+			url = "https://tr.rbxcdn.com/180DAY-5e5ca61c9d62b9690eb685071ace5341/768/432/Image/Webp/noFilter"
+		},
+
+		footer = {
+			text = "Lexs Hub Update • Reborn As Swordsman"
+		},
+
+		timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
+	}}
+}
+
+request({
+	Url = WEBHOOK,
+	Method = "POST",
+	Headers = {
+		["Content-Type"] = "application/json"
+	},
+	Body = HttpService:JSONEncode(data)
+})
